ea90cee41df203a4a38155c43f3b0e681edc2453
Эмиль <emil.aytaliev@mail.ru>
2022-04-13 12:22:41 +0300

    первичный вариант

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from utils import get_posts_all, get_posts_by_user, get_comments_by_post_id, get_post,search_for_posts, search_for_post
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    posts = get_posts_all()
+    return render_template('index.html', posts=posts, len=len(posts))
+
+
+@app.route("/post/<int:name>")
+def post(name):
+    posts = get_posts_by_user(name)
+    comment = get_comments_by_post_id(name)
+    return render_template('post.html', posts=posts, len=len(comment), comment=comment)
+
+
+@app.route("/user-feed/<user>")
+def feed(user):
+    feeds = get_post(user)
+    return render_template('user-feed.html', feeds=feeds)
+
+
+@app.route("/search/")
+def search():
+    search_by = request.args.get("s")
+    posts = search_for_posts(search_by)
+    return render_template('search.html', search_by=search_by, posts=posts, len=len(posts))
+
+
+
+
+app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,72 @@
+import json
+
+
+#Посты вместе с информацией
+def load_post():
+    with open('data/data.json', 'r', encoding='utf-8') as file:
+        posts = json.load(file)
+    return posts
+
+
+#комментарий
+def load_comment():
+    with open('data/comments.json', 'r', encoding='utf-8') as file:
+        comment = json.load(file)
+    return comment
+
+
+#возвращает посты
+def get_posts_all():
+
+    return load_post()
+
+
+#возвращает один пост по его идентификатору
+def get_posts_by_user(user_name):
+    post = []
+    for i in load_post():
+        if user_name == i['pk']:
+            post.append(i)
+    return post
+
+
+#возвращает посты определенного пользователя
+def get_post(user):
+    post = []
+    for i in load_post():
+        if user == i['poster_name']:
+            post.append(i)
+    return post
+
+
+#возвращает комментарии определенного поста
+def get_comments_by_post_id(post_id):
+    coment = []
+    for i in load_comment():
+        if post_id == i['post_id']:
+            coment.append(i)
+    return coment
+
+
+#возвращает список постов по ключевому слову
+def search_for_posts(query):
+    all_posts = [x for x in load_post() if query in x['content']]
+    return all_posts
+
+
+#возвращает один пост по его идентификатору
+def get_post_by_pk(pk):
+    for i in load_post():
+        if pk == i['pk']:
+            return {
+                'poster_name': i['poster_name'],
+                "poster_avatar": i["poster_avatar"],
+                "pic": i["pic"],
+                "content": i["content"],
+                "views_count": i["views_count"],
+                "likes_count": i["likes_count"],
+                "pk": i["pk"],
+            }
+
+
+
